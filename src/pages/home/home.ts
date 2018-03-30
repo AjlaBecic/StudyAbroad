b7cada1181f4b4a63dfa3ac0b8f64d2351e0fc6c
AjlaBecic <abecic1@etf.unsa.ba>
2018-03-30 18:53:13 +0200

    pocetna stranica

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Nav, Platform, NavController } from 'ionic-angular';
+
+import { ListPage } from '../list/list';
+import { Proba } from '../proba/proba';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  pages: Array<{title: string, component: any}>;
 
   constructor(public navCtrl: NavController) {
+    this.pages = [
+      { title: 'List', component: ListPage },
+      { title: 'Proba', component: Proba }
+    ];
+  }
 
+  nextpage(page:string) {
+    //this.navCtrl.push(this.pages.find(item => item.title == page).component);
+    this.navCtrl.setRoot(this.pages.find(item => item.title == page).component);
   }
 
 }

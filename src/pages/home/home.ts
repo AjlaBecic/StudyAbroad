a2effd825b4368b33c8f7f9d3e8bce8cd3302078
AjlaBecic <abecic1@etf.unsa.ba>
2018-05-24 11:34:13 +0200

    eror

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -37,7 +37,7 @@ export class HomePage {
       location: 'default'
     }).then((db: SQLiteObject) => {
       this.jobs.push({id:105,firma:"connection success",naslov:"ss",grad:"ss"})
-        /*db.executeSql('CREATE TABLE IF NOT EXISTS jobs(id INTEGER PRIMARY KEY, firma TEXT, objava TEXT, trajanje TEXT, opis TEXT, oblast TEXT, grad TEXT)', {})
+        db.executeSql('CREATE TABLE IF NOT EXISTS jobs(id INTEGER PRIMARY KEY, firma TEXT, objava TEXT, trajanje TEXT, opis TEXT, oblast TEXT, grad TEXT)', {})
        .then(res => {this.jobs.push({id:100,firma:"database success",naslov:"ss",grad:"ss"})})
        .catch(e => {this.jobs.push({id:101,firma:"database unsuccess",naslov:"ss",grad:"ss"})});
     
@@ -52,8 +52,8 @@ export class HomePage {
             this.jobs.push({id:res.rows.item(i).id,firma:res.rows.item(i).firma,naslov:res.rows.item(i).objava,grad:res.rows.item(i).grad})
           }
         })
-        .catch(e => {this.jobs.push({id:104,firma:"select success",naslov:"ss",grad:"ss"})});*/
-      }).catch(e => {this.jobs.push({id:105,firma:"connection unsuccess",naslov:"ss",grad:"ss"})});
+        .catch(e => {this.jobs.push({id:104,firma:"select success",naslov:"ss",grad:"ss"})});
+      }).catch(e => {this.jobs.push({id:105,firma:e,naslov:"ss",grad:"ss"})});
   }
 
 }

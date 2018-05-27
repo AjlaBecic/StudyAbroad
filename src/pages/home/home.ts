4588bf5f2b52bfd2be86edda11be9c736d3f293b
AjlaBecic <abecic1@etf.unsa.ba>
2018-05-27 19:34:59 +0200

    joboffer dodavanje u bazu14

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -36,7 +36,6 @@ export class HomePage {
       name: 'ionicdb.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('DROP TABLE jobs',{}).then(res=>{}).catch(e=>{});
         db.executeSql('CREATE TABLE IF NOT EXISTS jobs(id INTEGER PRIMARY KEY, firma TEXT, trajanje TEXT, oblast TEXT, grad TEXT, telefon TEXT, email TEXT, naslov TEXT, link TEXT)', {})
        .then(res => {this.jobs.push({id:100,firma:"database success",naslov:"ss",grad:"ss"})})
        .catch(e => {this.jobs.push({id:101,firma:"database unsuccess",naslov:"ss",grad:"ss"})});
@@ -44,6 +43,13 @@ export class HomePage {
        db.executeSql('CREATE TABLE IF NOT EXISTS scolarship(id INTEGER PRIMARY KEY, naslov TEXT, ciklus TEXT, trajanje TEXT, oblast TEXT, grad TEXT, konkurs NUMBER)', {})
        .then(res => {this.jobs.push({id:100,firma:"database success",naslov:"ss",grad:"ss"})})
        .catch(e => {this.jobs.push({id:101,firma:"database unsuccess",naslov:"ss",grad:"ss"})});
+
+       db.executeSql("INSERT INTO jobs(firma, trajanje, oblast, grad, telefon, email, naslov, link) VALUES('aa' , 'aa', 'aa' , 'aa' , 'aa' ,'aa' ,'aa' , 'aa' )" , {})
+       .then(res => {
+                   
+         //this.navCtrl.setRoot(Job);
+       })
+       .catch(e => {       })
     
         /*db.executeSql("INSERT INTO scolarship (naslov, ciklus, trajanje, oblast, grad, konkurs) VALUES ('Konkurs za dodjelu Erasmus stipendija baza', 'Master', 'Dva semestra', 'IT', 'Graz', 2)", {})
         .then(res => {this.jobs.push({id:102,firma:"insert success",naslov:"ss",grad:"ss"})})

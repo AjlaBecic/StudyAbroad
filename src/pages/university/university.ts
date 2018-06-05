d52d478c0d104779d081fbbf4fb92cae67e7d15c
AjlaBecic <abecic1@etf.unsa.ba>
2018-06-06 00:03:01 +0200

    lokacija6

--- src/pages/university/university.ts
+++ src/pages/university/university.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
-import {
-    GoogleMaps,
-    GoogleMap,
-    GoogleMapsEvent,
-    GoogleMapOptions,
-    CameraPosition,
-    MarkerOptions,
-    Marker
-  } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent } from '@ionic-native/google-maps';
 
 @Component({
     selector: 'page-university',
@@ -30,31 +22,11 @@ import {
     
     loadMap() {
         
-            let mapOptions: GoogleMapOptions = {
-              camera: {
-                 target: {
-                   lat: 43.0741904,
-                   lng: -89.3809802
-                 },
-                 zoom: 18,
-                 tilt: 30
-               }
-            };
-        
-            this.map = GoogleMaps.create('map_canvas', mapOptions);
-        
-            let marker: Marker = this.map.addMarkerSync({
-              title: 'Ionic',
-              icon: 'blue',
-              animation: 'DROP',
-              position: {
-                lat: 43.0741904,
-                lng: -89.3809802
-              }
-            });
-            marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-              alert('clicked');
-            });
+            // Create a map after the view is ready and the native platform is ready.
+    this.map = GoogleMaps.create('map_canvas');
+    
+        // No longer wait GoogleMapsEvent.MAP_READY event
+        // ( except you use map.getVisibleRegion() )
           }
   
 }

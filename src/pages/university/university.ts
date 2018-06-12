46248a742159f0e372b1b7729bf364ff63510d64
AjlaBecic <abecic1@etf.unsa.ba>
2018-06-12 17:32:49 +0200

    lokacija21

--- src/pages/university/university.ts
+++ src/pages/university/university.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { GoogleMaps, GoogleMap, GoogleMapsEvent } from '@ionic-native/google-maps';
@@ -29,5 +29,58 @@ import { ViewChild } from '@angular/core/src/metadata/di';
     
     }
   
-}
+  }*/
+
+  import {
+    GoogleMaps,
+    GoogleMap,
+    GoogleMapsEvent,
+    GoogleMapOptions,
+    CameraPosition,
+    MarkerOptions,
+    Marker
+  } from '@ionic-native/google-maps';
+  import { Component } from "@angular/core/";
+  
+  @Component({
+    selector: 'page-home',
+    templateUrl: 'home.html'
+  })
+  export class University {
+    map: GoogleMap;
+    constructor() { }
+  
+    ionViewDidLoad() {
+      this.loadMap();
+    }
+  
+    loadMap() {
+  
+      let mapOptions: GoogleMapOptions = {
+        camera: {
+           target: {
+             lat: 43.0741904,
+             lng: -89.3809802
+           },
+           zoom: 18,
+           tilt: 30
+         }
+      };
+  
+      this.map = GoogleMaps.create('map_canvas', mapOptions);
+  
+      let marker: Marker = this.map.addMarkerSync({
+        title: 'Ionic',
+        icon: 'blue',
+        animation: 'DROP',
+        position: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        }
+      });
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+        alert('clicked');
+      });
+    }
+  }
   

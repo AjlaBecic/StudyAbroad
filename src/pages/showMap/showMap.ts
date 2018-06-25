9119450bb377f1cfd4b43259f28a681c4a3cf31c
AjlaBecic <abecic1@etf.unsa.ba>
2018-06-25 22:49:56 +0200

    geoloc4

--- src/pages/showMap/showMap.ts
+++ src/pages/showMap/showMap.ts
@@ -41,7 +41,6 @@ import { Geolocation } from '@ionic-native/geolocation';
           center: this.location,
           zoom: 15
         }
-        window.alert(resp.coords.latitude);
   
         this.map = new google.maps.Map(this.mapRef.nativeElement, options);
   
@@ -82,15 +81,15 @@ import { Geolocation } from '@ionic-native/geolocation';
     }
 
     getDistance(lat1, lon1, lat2, lon2) {
-      var R = 6371; // Radius of the earth in km
-      var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+      var R = 6371;
+      var dLat = this.deg2rad(lat2-lat1);
       var dLon = this.deg2rad(lon2-lon1); 
       var a = 
         Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
         Math.sin(dLon/2) * Math.sin(dLon/2); 
       var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      this.distance = (R * c).toFixed(2); // Distance in km
+      this.distance = (R * c).toFixed(2);
     }
   
     deg2rad(deg) {
